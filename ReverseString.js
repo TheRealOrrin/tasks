5dbc11a3283a3769eb19f311ee4d4024d2fb21a9
Orrin Duin <orrin.duin@gmail.com>
2018-05-30 22:40:01 +0200

    #2

---
+++ ReverseString.js
@@ -0,0 +1,25 @@
+/* Using the JavaScript language, have the function
+FirstReverse(str) take the str parameter being passed
+and return the string in reversed order. For example:
+if the input string is "Hello World and Coders" then
+your program should return the string sredoC dna dlroW
+olleH. */
+
+
+
+function ReverseString(str) { 
+const separatedString = str.split("");
+alert(separatedString);
+
+
+    // code goes here  
+    // return str; 
+string.FirstReverse           
+  }
+     
+  // keep this function call here 
+  FirstReverse(readline());
+  ReverseString("abcd");
+
+
+
