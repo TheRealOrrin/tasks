e1bd550cdf9e0454a852009132f6b60be9a27747
Orrin Duin <orrin.duin@gmail.com>
2018-05-31 21:25:40 +0200

    Program finished to reverse a string.

--- ReverseString.js
+++ ReverseString.js
@@ -6,7 +6,17 @@ your program should return the string sredoC dna dlroW
 olleH. */
 
 function ReverseString(str) { 
-const separatedString = str.split("");
-  }
+  const separatedString = str.split("");
+  let reversedString = separatedString[0];
 
-ReverseString("abcd");
+  for(let i = 1; i < separatedString.length; i++) {
+    reversedString = separatedString[i] + reversedString;
+  }
+return reversedString;
+}
+
+let result1 = ReverseString("abcd");
+console.log(result1);
+let result2 = ReverseString("Расцветали яблони и груши, Поплыли туманы над рекой, Выходила на берег Катюша, На высокий берег, на крутой.");
+console.log(result2);
+
