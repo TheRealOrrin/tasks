3fd5da961d7d8020d0d0e8edb9f2656a8ee43dc3
Orrin Duin <orrin.duin@gmail.com>
2018-06-05 22:29:39 +0200

    Almost finished, just have to check its functioning in a browser.

--- LetterChanger.js
+++ LetterChanger.js
@@ -1,6 +1,5 @@
 function letterChanger(str) {
   let separatedCharacters = str.split("");
-  
   const alphaBet = ["", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
    "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
@@ -10,14 +9,14 @@ for(let i = 0; i < separatedCharacters.length; i++) {
 
 function changeCharacters(character) {
   let middleCharacter = character.toLowerCase();
-  console.log(middleCharacter);
   let switchedCharacter;
 
   if (alphaBet.includes(middleCharacter)) {
     let characterIndex = alphaBet.indexOf(middleCharacter);
     characterIndex = characterIndex + 1;
+    if (characterIndex == 27) { characterIndex = 1;}
     middleCharacter = alphaBet[characterIndex];
- 
+     
     if (character == character.toLowerCase()) {
       switchedCharacter = middleCharacter.toUpperCase();
     }
@@ -37,5 +36,9 @@ return separatedCharacters.join("");
 
 }
 
-let result = letterChanger("AbCd&z");
-console.log(result); 
+//let result = letterChanger("AbCd&z");
+//console.log(result); 
+
+let someString = prompt("Please enter the string you want to change", "Type here");
+
+alert(`The result of this function upon your string "${someString}" is this string: ${letterChanger(someString)}.`);
