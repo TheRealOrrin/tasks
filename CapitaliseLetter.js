cd40bc056ea7c7ed1dd4d7e46be3517afad26514
Orrin Duin <orrin.duin@gmail.com>
2018-06-09 00:06:02 +0200

    Dealt with space

--- CapitaliseLetter.js
+++ CapitaliseLetter.js
@@ -4,18 +4,24 @@ const appDiv = document.getElementById('app');
 const input  = document.getElementById('input');
 
 function capitaliseLetter(str) {
-    const separatedWords = str.split(" ");
+  const separatedWords = str.split(" ");    
     
-    for(let i = 0; i < separatedWords.length; i++) {
-      let firstLetter = separatedWords[i].split("", 1);
-      let restOfWord = separatedWords[i].slice(1);
-      let firstLetterUpperCase = firstLetter[0].toUpperCase();
-      separatedWords[i] = firstLetterUpperCase.concat(restOfWord);
+  for(let i = 0; i < separatedWords.length; i++) {
+      
+    let firstLetter = separatedWords[i].split("", 1);
+    let restOfWord = separatedWords[i].slice(1);
+    let firstLetterUpperCase = " ";
+
+    if(firstLetter[0] != undefined) {
+      firstLetterUpperCase = firstLetter[0].toUpperCase();
       }
-    return separatedWords.join(" ");
+    
+    separatedWords[i] = firstLetterUpperCase.concat(restOfWord);
+    
     }
+  return separatedWords.join(" ");
+}
 
 input.addEventListener('input', function() {
   appDiv.innerHTML = capitaliseLetter(input.value);
 })
-      
