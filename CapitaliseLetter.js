27f5004786b62fa8659b70abb6d3e6cb28036a8f
Orrin Duin <orrin.duin@gmail.com>
2018-06-08 23:37:50 +0200

    Added 'let' and HTML-"interface"

--- CapitaliseLetter.js
+++ CapitaliseLetter.js
@@ -1,16 +1,21 @@
+import './style.css';
+
+const appDiv = document.getElementById('app');
+const input  = document.getElementById('input');
+
 function capitaliseLetter(str) {
     const separatedWords = str.split(" ");
     
-    for(i = 0; i < separatedWords.length; i++) {
+    for(let i = 0; i < separatedWords.length; i++) {
       let firstLetter = separatedWords[i].split("", 1);
       let restOfWord = separatedWords[i].slice(1);
-      firstLetterUpperCase = firstLetter[0].toUpperCase();
+      let firstLetterUpperCase = firstLetter[0].toUpperCase();
       separatedWords[i] = firstLetterUpperCase.concat(restOfWord);
       }
-  
-  return separatedWords.join(" ");
-  
-  }
-  
-  result = capitaliseLetter("hello world");
-  console.log(result);
+    return separatedWords.join(" ");
+    }
+
+input.addEventListener('input', function() {
+  appDiv.innerHTML = capitaliseLetter(input.value);
+})
+      
