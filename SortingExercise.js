affe1884c4324801fa0820d3af93ef305f0e13c6
Orrin Duin <orrin.duin@gmail.com>
2018-06-14 20:19:08 +0200

    Working algorithm, made in StackBlitz at work :)

---
+++ SortingExercise.js
@@ -0,0 +1,46 @@
+import './style.css';
+
+const appDiv = document.getElementById('app');
+const input  = document.getElementById('input');
+
+function sortingExercise(str) {
+  let looseCharArray=str.split("");
+  
+  for(let i = 0; i < looseCharArray.length; i++) {
+
+    for(let i = 0; i < looseCharArray.length; i++) {
+      if(looseCharArray[i] > looseCharArray[i + 1]) {
+        looseCharArray = (swapPlace(i + 1, i, looseCharArray));
+        console.log(looseCharArray);
+      }
+    }
+
+  }
+
+return looseCharArray.join(""); 
+
+}
+
+function swapPlace(smaller, bigger, arrayBeforeSwap) {
+  let arrayAfterSwap = arrayBeforeSwap;
+  let smallerOne = arrayBeforeSwap[smaller];
+  let biggerOne = arrayBeforeSwap[bigger];
+
+  arrayAfterSwap[smaller] = biggerOne;
+  arrayAfterSwap[bigger] = smallerOne;
+
+  return arrayAfterSwap;
+}
+
+input.addEventListener('input', function() {
+    appDiv.innerHTML = sortingExercise(input.value);
+})
+
+//OD, 14-06-2018
+
+/* Using the JavaScript language, have the function AlphabetSoup(str)
+take the str string parameter being passed and return the string with
+the letters in alphabetical order (ie. hello becomes ehllo). Assume
+numbers and punctuation symbols will not be included in the string. */
+
+//No shortcuts now.
